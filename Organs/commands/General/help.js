1b078cc9d39eb71dfc291264cb53266ddc094a9f
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-06-29 18:34:38 +0530

    Update help.js
    Updated Help section image

--- Organs/commands/General/help.js
+++ Organs/commands/General/help.js
@@ -86,19 +86,19 @@ Here's the list of my Commands.*\n\n`;
       txt += `📗 Type *${prefix}help* <Command-Name> or <Command-Name> --info\n\n`;
       txt += `*🎐Ronen-Bots*`
       const eternity = [
-        "https://images3.alphacoders.com/107/1078895.png",
-        "https://images6.alphacoders.com/103/1037400.png",
-        "https://images6.alphacoders.com/102/1020806.jpg",
-        "https://images7.alphacoders.com/104/1040192.jpg",
-        "https://images5.alphacoders.com/104/1045349.jpg",
-        "https://images6.alphacoders.com/102/1024472.png",
-        "https://images8.alphacoders.com/120/1206401.jpg",
-        "https://images8.alphacoders.com/122/1222661.png",
-        "https://images3.alphacoders.com/120/1207252.jpg",
-        "https://images2.alphacoders.com/125/1258571.jpg",
-        "https://images7.alphacoders.com/125/1258568.jpg",
-        "https://images2.alphacoders.com/125/1258571.jpg",
-        "https://images2.alphacoders.com/125/1254189.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
+        "https://i.ibb.co/2gdVrky/20230210-160408.jpg",
       ];
       const eternitylogo =
         eternity[Math.floor(Math.random() * eternity.length)];
