509da4e3455ef36abd5be6818cf545611c44b30e
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-06-29 18:40:30 +0530

    Update hi.js
    Updated Hii command to Info command

--- Organs/commands/General/hi.js
+++ Organs/commands/General/hi.js
@@ -1,15 +1,27 @@
 module.exports = {
-    name: "hi",
-    alias: ["hello"],
-    desc: "Tells hello",
+    name: "info",
+    alias: ["ronen,elaina,bot"],
+    desc: "Gives you a brief description on bot and it's developer",
     cool:3,
-    react:"✅",
+    react:"🎐",
     category: "General",
     start: async(client, m,{pushName}) => {
         if(m.quoted){
             const nme= await client.username(m.quoted.sender)
             console.log(nme)
-            await client.sendMessage(m.from,{text:`Hello ${nme}`},{quoted:m})
+            await client.sendMessage(m.from,{text:`Hii! ${nme} 
+            I'm Elaina a whatsapp bot, Developed and Reverse engineered by Ronen to make your whatsapp more enjoyable and to do everything possible on whatsapp.
+        
+💜 *Our official support group* : http://surl.li/eumln
+🐨 *My YouTube* : https://youtube.com/c/RonenAMVs
+🔖 *Website* : Coming soon...
+*About Ronen💜*
+*🎗️Name* : Ronen Singha
+*🧧Age* : Immortal heheh✨
+*♦️Country* : India🇮🇳
+*🎯Gender* : Male👨‍🦱
+*❣️Relationship status* : Single
+*🎏Hobbies* : Coding, Playing Guitar, Video Editing, Workout 💪, Love Watching Animes and Kdramas`},{quoted:m})
 
         }else{
         await client.sendMessage(m.from,{text:`Hello ${pushName}`},{quoted:m})
