9ad61c6cb556e2bd9327b1c49c1c8c09e251e12c
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-06-29 19:54:39 +0530

    Update hi.js

--- Organs/commands/General/hi.js
+++ Organs/commands/General/hi.js
@@ -13,7 +13,7 @@ module.exports = {
           _*----⚠️[Rule]🚫⚠️----*_\n\n❌ *RULES SHOULD BE FOLLOWED STRICTLY* 🚫\n*•* use *.support* to get the Official group link in your dm\n*•* Don't chat in private with bot, you'll be blocked soon if you do \n*•* Dont use wrong command, use the command given in the *help list* \n*•* Dont spam the bot with commands if the bot is not responding, it means the bot is maybe offline or facing internet issue. \n*•* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫 `},{quoted:m})
 
         }else{
-        await client.sendMessage(m.from,{text:`Hello ${pushName}`},{quoted:m})
+        await client.sendMessage(m.from,{text:`Hello ${pushName}\n_*----⚠️[Rule]🚫⚠️----*_\n\n❌ *RULES SHOULD BE FOLLOWED STRICTLY* 🚫\n*•* use *.support* to get the Official group link in your dm\n*•* Don't chat in private with bot, you'll be blocked soon if you do \n*•* Dont use wrong command, use the command given in the *help list* \n*•* Dont spam the bot with commands if the bot is not responding, it means the bot is maybe offline or facing internet issue. \n*•* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫 `},{quoted:m})
         }
     }
 }
