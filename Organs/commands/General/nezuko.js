431def47966be792012f362ceb39d3e7bf5dbeed
X-sensei <77637156+X-sensei@users.noreply.github.com>
2023-06-30 08:09:24 +0530

    Update nezuko.js

--- Organs/commands/General/nezuko.js
+++ Organs/commands/General/nezuko.js
@@ -1,8 +1,8 @@
 require('../../../settings')
 const axios = require('axios')
 module.exports = {
-    name: "info",
-    alias: ["elaina,bot,ronen"],
+    name: "truth",
+    alias: ["t"],
     usage: `${prefa}info`,
     desc: "Will send you details of our bot",
     react: "🎐",
@@ -15,28 +15,17 @@ module.exports = {
         args
     }) => {
 let yup = [
-            'https://i.ibb.co/3vNvjGc/PXL-20230225-211608710.jpg',
-            'https://i.ibb.co/3vNvjGc/PXL-20230225-211608710.jpg',
-            'https://i.ibb.co/3vNvjGc/PXL-20230225-211608710.jpg',
-            'https://i.ibb.co/3vNvjGc/PXL-20230225-211608710.jpg',
-            'https://i.ibb.co/3vNvjGc/PXL-20230225-211608710.jpg',
-            'https://i.ibb.co/3vNvjGc/PXL-20230225-211608710.jpg'
+            'hii',
+            'yo',
+            'test',
+            'test2',
+            'test3',
+            'test4'
           ]
 let nezuko = await axios.get('https://api.github.com/repos/EternityBots/Nezuko')
 let rae = yup[Math.floor(Math.random() * yup.length)]
 
-var rr = `🎍 _*𝙀𝙇𝘼𝙄𝙉𝘼 𝘽𝙊𝙏*_  🎍\n\n🍀Hii! I'm Elaina a whatsapp bot, Developed and Reverse engineered by *Ronen* to make your whatsapp more enjoyable and to do everything possible on whatsapp.
-        
-💜 *Our official support group* : http://surl.li/eumln
-🐨 *My YouTube* : https://youtube.com/c/RonenAMVs
-🔖 *Website* : Coming soon...
-*About Ronen💜*
-*🎗️Name* : Ronen Singha
-*🧧Age* : Immortal heheh✨
-*♦️Country* : India🇮🇳
-*🎯Gender* : Male👨‍🦱
-*🎭Business no* : https://wa.me/15069751205
-*🎏Hobbies* : Coding, Playing Guitar, Video Editing, Boxing, Workout 💪, Love Watching Animes and Kdramas\n`
-await client.sendMessage(m.from,{image:{url:rae}, caption:rr},{quoted:m})
+var rr = `https://i.ibb.co/3vNvjGc/PXL-20230225-211608710.jpg`
+await client.sendMessage(m.from,{image:{url:rr}, caption:rae},{quoted:m})
     }
 }
